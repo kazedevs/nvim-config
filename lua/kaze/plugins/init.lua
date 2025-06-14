fc567e00587c4880e54695d5d7b18c9a60991655
kazedevs <businessrajkumardas@gmail.com>
2025-06-14 11:47:21 +0530

    fix: working LSP config with manual lsp setup

--- lua/kaze/plugins/init.lua
+++ lua/kaze/plugins/init.lua
@@ -1,4 +1,24 @@
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"nvim-lua/plenary.nvim",
+	"christoomey/vim-tmux-navigator",
+
+	-- Mason for managing LSP servers
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+
+	-- Mason-LSPConfig (no config block here — it's handled in lspconfig.lua)
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+	},
+
+	-- LSPConfig (your lspconfig.lua already handles the config)
+	{
+		"neovim/nvim-lspconfig",
+	},
 }
